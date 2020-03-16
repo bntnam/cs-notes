6d311985b3629230c38f8f53e0b6066070f50611
bntnam <bntnam.app@gmail.com>
2020-03-16 22:12:49 +0100

    Added Insertion Sort test

--- docs/js/sortingAlgorithms/insertionSort/index.js
+++ docs/js/sortingAlgorithms/insertionSort/index.js
@@ -1,14 +1,12 @@
 function insertionSort(arr) {
   for (let i = 1; i < arr.length; i++) {
     let currentVal = arr[i];
-    for (let j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
-      arr[j+1] = arr[j];
+    for (var j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
+      arr[j + 1] = arr[j];
     }
-    arr[j+1] = currentVal;
+    arr[j + 1] = currentVal;
   }
   return arr;
 }
 
-console.log(
-  insertionSort([3, 44, 38, 5, 47, 15, 36, 26, 27, 2, 46, 4, 19, 50, 48])
-);
+module.exports = insertionSort;
